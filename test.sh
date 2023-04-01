7db5b9ac3bff6d2f31a6f2b4e3727714aa9e7dde
Yuxin Jin <84665930+yuxinjin-whu@users.noreply.github.com>
2023-04-01 11:28:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 
+# This is evaluation of D-DFFNet training on CUHK-TR-1 dataset, if you plan to evaluate other model, please change the pretrained path.
+
 python test.py --pretrained '/data/jyx/dbd/D-DFFNet/checkpoint/Ablation_study/DFFNet_ablation/D-DFFNet.pth' --test_dataset 'Shi'
 python test.py --pretrained '/data/jyx/dbd/D-DFFNet/checkpoint/Ablation_study/DFFNet_ablation/D-DFFNet.pth' --test_dataset 'CTCUG'
 python test.py --pretrained '/data/jyx/dbd/D-DFFNet/checkpoint/Ablation_study/DFFNet_ablation/D-DFFNet.pth' --test_dataset 'DUT'
